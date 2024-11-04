a814300e4abdf7fdfc4066b23afce3980e72cd3f
Salma-mohmed-Abdi <salmosaafi46@gmail.com>
2024-11-04 23:25:51 +0300

    Add files via upload

--- taskControllers.js
+++ taskControllers.js
@@ -9,3 +9,21 @@ exports.getTasks = (req, res) => {
     res.end(JSON.stringify(tasks))
 }
 
+exports.createTask = (req, res) => {
+    const form = new IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            res.writeHead(400, { 'content-type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Error parsing form'
+            }))
+            return;
+        }
+
+        if (!fields.title) {
+            res.writeHead(400, { 'content-type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Title is required'
+            }))
+            return;
+        }
